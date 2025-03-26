0e49837b82bb47b739c04e1d28a611127cf6526d
TRichardVD <150446958+TRichardVD@users.noreply.github.com>
2025-03-26 08:39:06 +0100

    add a gadget with name, picture and price (step 2 completed)

--- vue-gadget-shop/main.js
+++ vue-gadget-shop/main.js
@@ -4,6 +4,9 @@ const app = Vue.createApp({
             title: 'Vue Gadget Shop',
             description:
                 'Découvrez les derniers gadgets électroniques de haute technologie !',
+            name: 'Smartphone XZ',
+            price: 799,
+            img: './assets/phone.jpg',
         };
     },
 });
