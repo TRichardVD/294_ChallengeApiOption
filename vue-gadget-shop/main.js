55e07a666fb0b13602f3fa6213847a50e308e16e
TRichardVD <150446958+TRichardVD@users.noreply.github.com>
2025-03-26 08:29:13 +0100

    feat : create the base of the website (step 1 completed)

---
+++ vue-gadget-shop/main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            title: 'Vue Gadget Shop',
+            description:
+                'Découvrez les derniers gadgets électroniques de haute technologie !',
+        };
+    },
+});
