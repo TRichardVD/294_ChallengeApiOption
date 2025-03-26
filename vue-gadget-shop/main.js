c81d2638779b5f3f249291e2ac113636a721fb8b
TRichardVD <150446958+TRichardVD@users.noreply.github.com>
2025-03-26 09:20:21 +0100

    add more gadgets (step 4 completed)

--- vue-gadget-shop/main.js
+++ vue-gadget-shop/main.js
@@ -4,10 +4,26 @@ const app = Vue.createApp({
             title: 'Vue Gadget Shop',
             description:
                 'Découvrez les derniers gadgets électroniques de haute technologie !',
-            name: 'Smartphone XZ',
-            price: 799,
-            img: './assets/phone.jpg',
-            inStock: true,
+            gadgets: [
+                {
+                    name: 'Smartphone XZ',
+                    price: 799,
+                    img: './assets/phone.jpg',
+                    inStock: true,
+                },
+                {
+                    name: 'Laptop pro',
+                    price: 1299,
+                    img: './assets/laptop.jpg',
+                    inStock: false,
+                },
+                {
+                    name: 'Ecouteurs bluetooth',
+                    price: 199,
+                    img: './assets/earbuds.jpg',
+                    inStock: true,
+                },
+            ],
         };
     },
 });
