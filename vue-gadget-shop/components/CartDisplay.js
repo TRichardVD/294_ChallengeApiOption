7002877c075b11bb77da6a5482b0dbd03b79bf5f
TRichardVD <150446958+TRichardVD@users.noreply.github.com>
2025-04-01 15:20:18 +0200

    refactor cart display into a separate component (step 8)

---
+++ vue-gadget-shop/components/CartDisplay.js
@@ -0,0 +1,37 @@
+app.component('cart-display', {
+    template:
+        /*html*/
+        `
+    <div class="cart">
+                <h2>🛒 Mon Panier ({{cart.length}})</h2>
+                <ul v-for="(product, index) in cart">
+                    <li>
+                        {{product.name}} - {{product.price}}€
+                        <button
+                            class="remove-btn"
+                            @click="removeFromCart(index)"
+                        >
+                            ❌
+                        </button>
+                    </li>
+                </ul>
+                <p v-show="cart.length === 0">Votre panier est vide.</p>
+                <h3 style="color: #007bff">
+                    💰 Total :
+                    <span v-if="cartTotal < 1000">{{cartTotal}}&euro;</span>
+                    <span v-else>
+                        <s style="color: red">{{cartTotal}}&euro;</s> ➝
+                        <strong style="color: green"
+                            >{{discountedTotal}}&euro; (-10%)</strong
+                        >
+                    </span>
+                </h3>
+            </div>
+    `,
+    props: ['cart', 'cartTotal', 'discountedTotal'],
+    methods: {
+        removeFromCart(index) {
+            this.$emit('remove-from-cart', index);
+        },
+    },
+});
